c3eb3d286fcb11f397f5c7f1d7eac868ef3c03ae
Vanson Wing Leung <vanson@vanportdev.com>
2024-05-23 18:27:19 +0800

    support broadcast udp signal

---
+++ home/fs/startup_fs_py_broadcast_udp_signal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/fs/fs-lattepanda-src/home/fs/pyfirm_testcar/ &&
+/home/fs/fs-lattepanda-src/home/fs/venv/bin/python3 \
+/home/fs/fs-lattepanda-src/home/fs/pyfirm_testcar/broadcast_udp_signal.py \
