dd85bed8596d5b275cde11c19d0e1c90fbee8f98
Vanson Wing Leung <vanson@vanportdev.com>
2024-05-23 19:27:34 +0800

    Update to support access to i2c

--- home/fs/startup_root.sh
+++ home/fs/startup_root.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sleep 5
+sudo chmod 777 /dev/i2c-0
+sudo chmod 777 /dev/i2c-1
+sudo chmod 777 /dev/i2c-2
+sudo chmod 777 /dev/i2c-3
+sudo chmod 777 /dev/i2c-4
+sudo chmod 777 /dev/i2c-5
+sudo chmod 777 /dev/i2c-6
+sudo chmod 777 /dev/i2c-7
 
 sleep 5
 cd /home/fs/fs-lattepanda-src/home/fs/simcom-cm/ && sudo ./simcom-cm 
